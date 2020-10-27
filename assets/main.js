08ffc6fbc217dff81aa9238a69afeaae23d1fb1f
foobball <foooobball@gmail.com>
2020-10-27 14:29:44 -0400

    lets the user know if it's out of date

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,20 @@
-document.getElementById('contact').addEventListener("click", () => {
+const vers = "2.2.0";
+
+document.getElementById('vers').innerText = `Version ${vers}`;
+/*document.getElementById('contact').addEventListener("click", () => {
     window.open(`https://discord.com/users/219541416760705024`);
-})
+})*/
 document.getElementById('github').addEventListener("click", () => {
     window.open(`https://github.com/foobball/MeetRejoin`);
 })
+fetch('https://raw.githubusercontent.com/foobball/MeetRejoin/main/vers.txt')
+    .then(res => res.text())
+    .then(body => {
+        console.log(body);
+        if (body !== vers) {
+            document.getElementById('author').innerHTML = 'by foob <a href="#" style="color:red" id="update">(Out of Date)</a>';
+            document.getElementById('update').addEventListener("click", () => {
+                window.open(`https://chrome.google.com/webstore/detail/bickjlhloaddmjnanlhdcoedebngamnn`);
+            })
+        }
+    })
