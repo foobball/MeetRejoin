8ce156a39128a8b84d76cb5d491c30fd7a40f61a
foobball <foooobball@gmail.com>
2020-10-27 12:55:16 -0400

    added github link

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ document.getElementById('contact').addEventListener("click", () => {
     window.open(`https://discord.com/users/219541416760705024`);
 })
 document.getElementById('github').addEventListener("click", () => {
-    window.open(`https://discord.com/users/219541416760705024`);
+    window.open(`https://github.com/foobball/MeetRejoin`);
 })
