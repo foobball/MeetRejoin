d2ada7a0a40fa4c2eb2cb8acfc4e2b0ad0838716
foobball <foooobball@gmail.com>
2020-10-27 13:49:24 -0400

    auto mute script

---
+++ mute.js
@@ -0,0 +1,18 @@
+let interval = window.setInterval(() => {
+    console.log(document.getElementsByClassName('sUZ4id').length);
+    if (document.getElementsByClassName('sUZ4id').length == 2) {
+        if (document.getElementsByClassName('sUZ4id')[0].children !== undefined) {
+            let i = 0;
+            let muting = setInterval(() => {
+                console.log('clicked');
+                console.log(document.getElementsByClassName('sUZ4id')[0].children[0]);
+                let muteButton = document.getElementsByClassName('sUZ4id')[0].children[0];
+                let camButton = document.getElementsByClassName('sUZ4id')[1].children[0];
+                if (muteButton.dataset.isMuted == "false") muteButton.click();
+                if (camButton.dataset.isMuted == "false") camButton.click();
+                if (i > 20) clearInterval(muting);
+            }, 100)
+            clearInterval(interval);
+        }
+    }
+}, 1000)
